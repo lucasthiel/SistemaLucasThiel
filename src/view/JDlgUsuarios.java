133a97b061e60c9674bafe018facff74cc2c1400
lucasthiel <lucas.thiel@estudante.ifms.edu.br>
2025-10-07 14:20:49 -0400

    Foi realizado o mapeamento com Hibernate no bean.

--- src/view/JDlgUsuarios.java
+++ src/view/JDlgUsuarios.java
@@ -23,7 +23,7 @@ public class JDlgUsuarios extends javax.swing.JDialog {
     public JDlgUsuarios(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        setTitle("Cadastro de Usuários");
+        setTitle("Cadastro do Usuário");
         setLocationRelativeTo(null);
         
         Util.habilitar(false, jTxtCodUsu, jTxtNomeUsu, jTxtApelidoUsu, jFmtCpfUsu, jFmtDataNascimentoUsu, jPwfSenhaUsu, jCboNivelUsu, jChbAtivoUsu, jBtnConfirmarUsu, jBtnCancelarUsu);
